53f50cbec685affb0bcb2f757623abd197dee881
devprashant <devprashant@gmail.com>
2016-07-21 12:26:29 +0530

    uploading to server

--- model/db.js
+++ model/db.js
@@ -2,6 +2,16 @@ var mongoose = require('mongoose');
 
 var dbName = "mongoosepm";
 var dbURI = "localhost:27017/"+ dbName;
+
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  dbURI = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function(){
